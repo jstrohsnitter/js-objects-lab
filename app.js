a007de2c8b933a010cabd85bb8ad0ab1cebfa1ab
jstrohsnitter <jstrohsnitter@gmail.com>
2024-10-20 18:27:21 -0400

    exercise 5

--- app.js
+++ app.js
@@ -42,4 +42,19 @@ Exercise 4
 Solve Exercise 4 here:
 */
 
+game.party = pokemon[3]
 
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+
+
+game.party = [game.party, pokemon[150], pokemon[149], pokemon[128], pokemon[64]]
+
+console.log(game)
