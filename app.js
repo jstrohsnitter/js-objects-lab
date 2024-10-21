84366843a7b685ca5ebaac6aba1bc255d01e7d6f
jstrohsnitter <jstrohsnitter@gmail.com>
2024-10-20 20:29:08 -0400

    Exercise 11

--- app.js
+++ app.js
@@ -118,3 +118,26 @@ Exercise 9
 Solve Exercise 9 here:
 */
 
+
+pokemon.forEach (theStarters => {
+  if (theStarters.starter === true)
+    console.log(theStarters.name)
+})
+
+/*
+Exercise 10
+Create a method called `catchPokemon` and add it to the `game` object. You should not need to edit the original game object directly. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 10 here:
+*/
+game.catchPokemon = function (pokemonObj){
+  game.party.push(pokemonObj)
+}
+game.catchPokemon(pokemon[23])
+console.log(game)
+
